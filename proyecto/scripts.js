6594ec42d3a2d4b3a26b86bb2704095ffa83e8d9
Bio121 <daniel.alejandro121@gmail.com>
2020-10-19 23:15:46 -0500

    navbar
    navbar casi responsiva, validaciones para inicio de sesión y registro (correo,nombre de usuario, contraseña de 8 caracteres con numero mayúscula y un carácter especial y teléfono de 10 dígitos) creación del archivo configuración de usuario (vació)

--- proyecto/scripts.js
+++ proyecto/scripts.js
@@ -15,4 +15,62 @@
                 window.location.href = "noticia.php";
             }
 
+function validacionRegistrarse() {
 
+    email = document.getElementById("emailRegistrarse").value;
+    if (!(/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(email))) {
+        alert("correo invalido");
+        return false;
+    }
+    
+    usuario = document.getElementById("usuarioRegistrarse").value;
+    if (usuario == null || usuario.length == 0 || /^\s+$/.test(usuario)) {
+        alert("agrege un usuario");
+        return false;
+    }   
+    
+    telefono = document.getElementById("telefonoRegistrarse").value;
+    if (!(/^\d{10}$/.test(telefono))) {
+        alert("numero de telefono no valido");
+        return false;
+    }
+    
+    pass = document.getElementById("contraseñaRegistrarse").value;
+    if (pass == null || pass.length < 8 || /^[a-zA-Z0-9]+$/.test(pass)) {
+        alert("agrege una contraseña valida");
+        return false;
+    }
+    
+    pass2 = document.getElementById("contraseñaConfirmarRegistrarse").value;
+    if (pass2 != pass) {
+        alert("confirme correctamente su contraseña");
+        return false;
+    }
+   
+    alert("Usted esta registrado" );
+
+}
+
+function validacionInicioSesion() {
+
+    email = document.getElementById("emailIniciarSesion").value;
+    if (!(/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(email))) {
+        alert("correo no registrado");
+        return false;
+    }
+    
+    usuario = document.getElementById("usuarioIniciarSesion").value;
+    if (usuario == null || usuario.length == 0 || /^\s+$/.test(usuario)) {
+        alert("usuario incorrecto");
+        return false;
+    }   
+    
+    pass = document.getElementById("contraseñaIniciarSesion").value;
+    if (pass == null || pass.length < 8 || /^[a-zA-Z0-9]+$/.test(pass)) {
+        alert("contraseña incorrecta");
+        return false;
+    }
+   
+    alert("Inicio de sesion exitoso" );
+
+}
